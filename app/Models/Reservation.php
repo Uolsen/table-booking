d24d08e0840d80e7b5a61a56fe76a00f68867128
stepanmocjak <stepan.mocjak@guava.cz>
2025-08-05 14:19:47 +0200

    feat: Reservation model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReservationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'people_count',
+        'start_time',
+        'end_time',
+    ];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+        'people_count' => 'integer',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Reservation;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'people_count' => $this->faker->numberBetween(1, 10),
+            'start_time' => $this->faker->dateTimeBetween('now', '+1 week'),
+            'end_time' => function (array $attributes) {
+                return $this->faker->dateTimeBetween(
+                    $attributes['start_time'],
+                    Carbon::parse($attributes['start_time'])->addHours(5)
+                );
+            },
+        ];
+    }
+}
