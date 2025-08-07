6ab2990f4961d2e9a432fe3ac93516c48fda4c8a
stepanmocjak <stepan.mocjak@guava.cz>
2025-08-07 13:59:44 +0200

    fix: Hero link

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -2,29 +2,42 @@
     <div class="relative">
         <div class="mx-auto max-w-7xl">
             <div class="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
-                <svg viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true" class="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block">
-                    <polygon points="0,0 90,0 50,100 0,100" />
+                <svg viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true"
+                     class="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block">
+                    <polygon points="0,0 90,0 50,100 0,100"/>
                 </svg>
 
                 <div class="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
                     <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
                         <div class="hidden sm:mb-10 sm:flex">
-                            <div class="relative rounded-full px-3 py-1 text-sm/6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                                Welcome to freshly opened Restaurant Experiment <a href="#" class="font-semibold whitespace-nowrap text-primary-600"><span aria-hidden="true" class="absolute inset-0"></span>Reserve your seat <span aria-hidden="true">&rarr;</span></a>
+                            <div
+                                class="relative rounded-full px-3 py-1 text-sm/6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                Welcome to freshly opened Restaurant Experiment <a href="#"
+                                                                                   class="font-semibold whitespace-nowrap text-primary-600"><span
+                                        aria-hidden="true" class="absolute inset-0"></span>Reserve your seat <span
+                                        aria-hidden="true">&rarr;</span></a>
                             </div>
                         </div>
-                        <h1 class="text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-7xl">Restaurant Experiment</h1>
-                        <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Welcome to Restaurant Experiment. You can easily reservate your table here.</p>
+                        <h1 class="text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-7xl">
+                            Restaurant Experiment</h1>
+                        <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Welcome to Restaurant
+                            Experiment. You can easily reservate your table here.</p>
                         <div class="mt-10 flex items-center gap-x-6">
-                            <x-button href="#"> Reservate </x-button>
-                            <a href="#" class="text-sm/6 font-semibold text-gray-900">Create an account <span aria-hidden="true">→</span></a>
+                            @if(auth()->check())
+                                <x-button href="{{ route('reservate') }}"> Reservate</x-button>
+                            @else
+                                <x-button href="{{ route('login') }}"> Reservate </x-button>
+                            @endif
+                            <a href="#" class="text-sm/6 font-semibold text-gray-900">Create an account <span
+                                    aria-hidden="true">→</span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            <img src="{{ Vite::asset('resources/img/restaurant.webp') }}" alt="Restaurant image" class="aspect-3/2 object-cover lg:aspect-auto lg:size-full" />
+            <img src="{{ Vite::asset('resources/img/restaurant.webp') }}" alt="Restaurant image"
+                 class="aspect-3/2 object-cover lg:aspect-auto lg:size-full"/>
         </div>
     </div>
 </div>
